f6848551365de0c3bc00bd9dd7937c81a19f295d
Takashiro Kazama <kazamanbou1209.pfm@gmail.com>
2022-01-08 03:35:47 +0900

    Fix refactoring business logic

--- app/controllers/csv_controller.rb
+++ app/controllers/csv_controller.rb
@@ -6,17 +6,16 @@ class CsvController < ApplicationController
 
   def create
     model_name = params[:model_name]
-    model = model_name.constantize
-
-    results = params[:results]
-
+    
     # results は JSON 形式の配列で渡ってくるので parse が必要
-    instances = results.map { |row_json| JSON.parse(row_json) }.map do |attributes|
-      model.new(attributes)
+    results = params[:results].map { |row_json| JSON.parse(row_json) }
+
+    instances = results.map do |attributes|
+      model_name.constantize.new(attributes)
     end
 
     # バルクインサート
-    model.import(instances)
+    model_name.constantize.import(instances)
 
     redirect_to csv_index_path, success: set_flash_message(model_name)
   end
@@ -24,11 +23,9 @@ class CsvController < ApplicationController
   def import
     file = params[:file]
     @model_name = params[:model_name]
-    model = @model_name.constantize
-    headers = set_headers(@model_name)
 
     # CsvImporter.run() の返り値は Hash 形式の配列
-    @results, @errors = CsvImporter.run(file, model, headers)
+    @results, @errors = CsvImporter.run(file, @model_name)
 
     respond_to do |format|
       if @errors.present?
@@ -40,32 +37,6 @@ class CsvController < ApplicationController
   end
 
   private
-
-    # モデルによって異なるヘッダーを設定
-    def set_headers(model_name)
-      case model_name
-      when "Author"
-        author_headers
-      when "Book"
-        book_headers
-      end
-    end
-
-    def author_headers
-      {
-        "名前" => "name",
-        "メールアドレス" => "email",
-        "生年月日" => "birthdate"
-      }
-    end
-
-    def book_headers
-      {
-        "作品名" => "title",
-        "価格" => "price",
-        "出版日" => "published_date"
-      }
-    end
 
     # モデルによって異なるフラッシュメッセージを設定
     def set_flash_message(model_name)
--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -1,28 +1,46 @@
 class CsvImporter
-  def self.run(file, model, headers)
-    results = [] # バリデーションチェックに通過した（= DBに取り込み可能な）データを格納する配列
-    errors = []  # CSVファイル読み込み中に生じたエラーを格納する配列
+  class << self
+    def run(file, model_name)
+      # モデルによってヘッダーを設定
+      headers = set_headers(model_name)
 
-    CSV.foreach(file.path, headers: true, skip_blanks: true).with_index(2) do |row, index|
-      # ヘッダーの項目に従って各値を切り出し
-      row_hash = row.to_hash.slice(*headers.keys)
-      attributes = row_hash.transform_keys(&headers.method(:[]))
+      results = [] # バリデーションチェックに通過した（= DBに取り込み可能な）データを格納する配列
+      errors = []  # CSVファイル読み込み中に生じたエラーを格納する配列
 
-      # バリデーションチェック（無効な値が入っていた場合はエラーを発生させる）
-      instance = model.new(attributes)
+      CSV.foreach(file.path, headers: true, skip_blanks: true).with_index(2) do |row, index|
+        # ヘッダーの項目に従って各値を切り出し
+        row_hash = row.to_hash.slice(*headers.keys)
+        attributes = row_hash.transform_keys(&headers.method(:[]))
 
-      if instance.valid?
-        results << attributes
-      else
-        # 何行目でエラーが生じたのかも記録
-        errors.push({ row_number: index, messages: instance.errors.full_messages })
+        # バリデーションチェック（無効な値が入っていた場合はエラーを発生させる）
+        instance = model_name.constantize.new(attributes)
+
+        if instance.valid?
+          results << attributes
+        else
+          # 何行目でエラーが生じたのかも記録
+          errors.push({ row_number: index, messages: instance.errors.full_messages })
+        end
+
+      rescue StandardError => e
+        errors.push({ row_number: index, messages: [e] })
+        return [], errors
       end
 
-    rescue StandardError => e
-      errors.push({ row_number: index, messages: [e] })
-      return [], errors
+      [results, errors]
     end
 
-    [results, errors]
+    private
+      
+      def set_headers(model_name)
+        headers = {}
+        
+        # locales ファイルに登録した値を key/value として使用
+        I18n.t("activerecord.attributes.#{model_name.underscore}").each do |k, v|
+          headers.store(v, k.to_s)
+        end
+
+        headers
+      end
   end
 end
